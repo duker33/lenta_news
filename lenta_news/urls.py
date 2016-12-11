f587841a060b1a5cc841aba13ee72047c65f5757
duker <duker33@gmail.com>
2016-12-11 17:30:31 +0300

    Render forms and pages

--- lenta_news/urls.py
+++ lenta_news/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.get_news_order),
+    url(r'^thanks/$', views.thanks_page),
 ]
---
+++ lenta_news/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .forms import OrderNewsForm
+
+
+def get_news_order(request):
+    if request.method == 'POST':
+        form = OrderNewsForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            date_to = form.cleaned_data['date_to']
+            date_from = form.cleaned_data['date_from']
+            print(email, date_to, date_from)
+            # TODO - send mail
+            # TODO - redirect page
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = OrderNewsForm()
+
+    return render(request, 'order_news.html', {'form': form})
+
+
+def thanks_page(request):
+    return render(request, 'thanks.html')
