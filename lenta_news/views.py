84f78acdd53928553e7fe46296b34af16739f820
duker <duker33@gmail.com>
2016-12-19 16:06:36 +0300

    Move form views to CBV

--- lenta_news/views.py
+++ lenta_news/views.py
@@ -1,30 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.views.generic.edit import FormView
 
 from lenta_news.forms import OrderNewsForm
 from lenta_news.services.mail_pdf import send_news_email
 
 
-# TODO - throw CBV
-def get_news_order(request):
-    if request.method == 'POST':
-        form = OrderNewsForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            date_to = form.cleaned_data['date_to']
-            date_from = form.cleaned_data['date_from']
-            result = send_news_email.delay(
-                email_to=email,
-                news_date_to=date_to,
-                news_date_from=date_from
-            )
-            result.get()
-            return HttpResponseRedirect('/thanks/')
-    else:
-        form = OrderNewsForm()
+class NewsOrderForm(FormView):
+    template_name = 'order_news.html'
+    form_class = OrderNewsForm
+    success_url = '/thanks/'
 
-    return render(request, 'order_news.html', {'form': form})
-
-
-def thanks_page(request):
-    return render(request, 'thanks.html')
+    def form_valid(self, form):
+        email = form.cleaned_data['email']
+        date_to = form.cleaned_data['date_to']
+        date_from = form.cleaned_data['date_from']
+        send_news_email.delay(
+            email_to=email,
+            news_date_to=date_to,
+            news_date_from=date_from
+        )
+        return super(FormView, self).form_valid(form)
